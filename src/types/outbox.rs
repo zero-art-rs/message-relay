8625ffdbbef63a45247eabf0e1adda006effc83b
serhii023 <serhii023@gmail.com>
2025-08-12 12:51:50 +0300

    update types

--- src/types/outbox.rs
+++ src/types/outbox.rs
@@ -1,27 +1,36 @@
 use chrono::{Utc, DateTime};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+use serde_with::{serde_as, base64::Base64};
 
+#[serde_as]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct MessageOutbox {
     /// The message content as binary data
+    #[serde_as(as = "Base64")]
     pub content: Vec<u8>, // binary string, Vec<u8>
     /// When the message was created
     pub created_at: DateTime<Utc>,
     /// Sequential number of this message in the chat
-    pub sequence_number: i64,
+    pub sequence_number: u32,
     /// Sequential number of epoch during which the message was sent
     pub epoch: u32,
     /// Unique identifier of the chat to send the message to.
     pub chat_id: Uuid,
 }
 
+#[serde_as]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ARTChangeOutbox {
     pub chat_id: Uuid,
+    #[serde_as(as = "Base64")]
     pub data: Vec<u8>,
     pub created_at: DateTime<Utc>,
-    pub sequence_number: i64,
+    pub sequence_number: u32,
+    #[serde_as(as = "Option<Base64>")]
     pub metadata: Option<Vec<u8>>,
+    #[serde_as(as = "Option<Base64>")]
     pub payload: Option<Vec<u8>>,
+    #[serde_as(as = "Base64")]
+    pub proof: Vec<u8>,
 }
